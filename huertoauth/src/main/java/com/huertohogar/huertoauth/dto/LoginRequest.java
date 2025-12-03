d2b662fbe842f5150b1e873c80efb73fc1f1acea
jose18042006 <jo.leiton@duocuc.cl>
2025-12-02 23:39:45 -0300

    Delete huertoauth directory

--- huertoauth/src/main/java/com/huertohogar/huertoauth/dto/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.huertohogar.huertoauth.dto;
-
-public class LoginRequest {
-    public String email;
-    public String password;
-
-}
--- huertoauth/src/main/java/com/huertohogar/huertoauth/dto/RegisterRequest.java
+++
@@ -1,7 +0,0 @@
-package com.huertohogar.huertoauth.dto;
-
-public class RegisterRequest {
-    public String nombre;
-    public String email;
-    public String password;
-}
